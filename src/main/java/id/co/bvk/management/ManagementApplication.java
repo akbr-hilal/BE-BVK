b20774b334bc0b69499870f2e5bf20f852c85df3
Hilal Akbar <94953711+akbr-hilal@users.noreply.github.com>
2024-10-17 07:36:42 +0000

    init apps

---
+++ src/main/java/id/co/bvk/management/ManagementApplication.java
@@ -0,0 +1,13 @@
+package id.co.bvk.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApplication.class, args);
+	}
+
+}
